d61002e1d3beb1a76bc2d269654be4032c3b4aad
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2022-11-17 21:24:56 -0300

    Create a default text for the state as example

---
+++ src/defaultText.ts
@@ -0,0 +1,16 @@
+export const defaultText =
+  ` # Main title
+
+  ## Project Desc
+
+  This project was made thanks with the help of [react-markdown](https://npmjs.com/react-markdown).
+
+  ## About it
+  This page, allows you to previsualize your markdown text you write down.
+
+  ### Technologies
+  * React
+  * and that would be it
+
+  Here the text continues...
+`
