271da123aaf92fbafd9e7117c6d7693900841683
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2022-11-19 23:38:35 -0300

    Add more text with markdown syntax as example

--- src/defaultText.ts
+++ src/defaultText.ts
@@ -12,5 +12,18 @@ export const defaultText =
   * React
   * and that would be it
 
-  Here the text continues...
+  __Here the text continues...__
+
+  ~~Strikethrough text~~
+
+  **Orderder list**
+  1. Item 1
+  2. Item 2
+  3. Item 3
+
+  **Unordered List**
+  - Item 1
+  - Item 2
+  - Item 3
+
 `
