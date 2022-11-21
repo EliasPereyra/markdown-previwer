f0a749e1b0327b3895762a2f93da6d1c85f60958
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2022-11-20 23:58:59 -0300

    Import the footer component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import remarkGfm from 'remark-gfm'
 import { defaultText } from './defaultText'
 
 import './App.css'
+import Footer from './components/Footer'
 
 function App() {
   const [content, setContent] = useState(defaultText)
@@ -14,26 +15,29 @@ function App() {
   }
 
   return (
-    <section>
-      <div>
-        <header>Editor</header>
-        <textarea
-          id="editor"
-          onChange={handleChange}
-          value={content}
-        ></textarea>
-      </div>
-
-      <div id="preview-container">
-        <header>Preview</header>
-        <div id="preview">
-          <ReactMarkdwon
-            children={content}
-            remarkPlugins={[remarkGfm]}
-          ></ReactMarkdwon>
+    <div>
+      <section>
+        <div>
+          <header>Editor</header>
+          <textarea
+            id="editor"
+            onChange={handleChange}
+            value={content}
+          ></textarea>
         </div>
-      </div>
-    </section>
+
+        <div id="preview-container">
+          <header>Preview</header>
+          <div id="preview">
+            <ReactMarkdwon
+              children={content}
+              remarkPlugins={[remarkGfm]}
+            ></ReactMarkdwon>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </div>
   )
 }
 
