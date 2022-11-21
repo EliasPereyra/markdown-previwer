4c5d941fb6ddbbfae3dab7f6dbf1bd020a655648
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2022-11-20 23:58:14 -0300

    Make a footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,18 @@
+import './styles.css'
+
+function Footer() {
+  return (
+    <footer>
+      <p>This project was made thanks to the react-markdown library.</p>
+      <p>You can check it out and give it a try.</p>
+      <p>
+        <a href="https://www.npmjs.com/package/react-markdown" target="_blank">
+          Here
+        </a>{' '}
+        it's the github repository:
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
