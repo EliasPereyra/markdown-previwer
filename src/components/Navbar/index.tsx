24b796144358ebc95c481d6346aa04ac49e3e8e4
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-08-20 23:43:04 -0300

    Feat: create a navbar component

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,17 @@
+import './styles.css'
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src="" alt="logo" />
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
